dd5e6fd7f14bbbdad8c725774510b563810f3b40
rahulkatoch99 <rahulkatoch99@gmail.com>
2022-07-13 11:05:11 +0530

    first commit

---
+++ src/Components/map.js
@@ -0,0 +1,53 @@
+import React from "react";
+import { GoogleMap, useJsApiLoader } from "@react-google-maps/api";
+
+const containerStyle = {
+  width: "100vw",
+  height: "100vh",
+};
+const style = {
+  width: "100%",
+  height: "100%",
+  position: "sticky",
+};
+
+function MapInfo(props) {
+  const center = {
+    lat: props.lat,
+    lng: props.lng,
+  };
+  const { isLoaded } = useJsApiLoader({
+    id: "google-map-script",
+    googleMapsApiKey: "AIzaSyBh-9v1iGONd6sOVoDlPPmv14VUEXE2ifg",
+  });
+
+  const [map, setMap] = React.useState(null);
+
+  const onLoad = React.useCallback(function callback(map) {
+    const bounds = new window.google.maps.LatLngBounds(center);
+    map.fitBounds(bounds);
+    setMap(map);
+  }, []);
+
+  const onUnmount = React.useCallback(function callback(map) {
+    setMap(null);
+  }, []);
+
+  return isLoaded ? (
+    <GoogleMap
+      mapContainerStyle={containerStyle}
+      center={center}
+      style={style}
+      zoom={16}
+      onLoad={onLoad}
+      onUnmount={onUnmount}
+    >
+      {/* Child components, such as markers, info windows, etc. */}
+      <></>
+    </GoogleMap>
+  ) : (
+    <></>
+  );
+}
+
+export default React.memo(MapInfo);
